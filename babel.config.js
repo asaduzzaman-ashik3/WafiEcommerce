98335f5ddaa1ba7f3ca74f4e93a059045713b4a0
Hridoy <hridoymia.prgmr@gmail.com>
2026-05-04 12:44:02 +0600

    remove reactCompiler experiment and add react-native-reanimated plugin

--- babel.config.js
+++ babel.config.js
@@ -6,7 +6,8 @@ module.exports = function (api) {
       "nativewind/babel",
     ],
       plugins: [
-      ["module:react-native-dotenv"]
+      ["module:react-native-dotenv"],
+      "react-native-reanimated/plugin",
     ],
   };
 };
